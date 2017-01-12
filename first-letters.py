48d224d2102dbfcf298884eded365484aade04d0
thegodeater <heer-n@hotmail.com>
2017-01-12 16:15:14 +0100

    Update first-letters.py

--- first-letters.py
+++ first-letters.py
@@ -4,6 +4,7 @@ def load_data_set("Namen_vrouw_per_10_jaar.csv":
         f.readline()
         return [[float(x) for x in row] for row in csv.reader(f+ delimiter=';')])
  data =load_data_set("Namen_vrouw_per_10_jaar.csv")
+        # lolz
 
 #gi
  # dit sorteert alle lijsten op het nummer voor 1900
